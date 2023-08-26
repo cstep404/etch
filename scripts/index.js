d2316f97a396d34701bceeb0b95fa3a4b04dabb7
Chip <cstep772@gmail.com>
2023-08-26 15:44:43 -0400

    Created 16 x 16 grid

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,46 @@ const mainContainerSelector = document.querySelector('#main-container');
 
 // create the element, manipulate the element, and then place it on page by appending
 
-// create 16 rows
-for (let i = 1; i <= 16; i++) {
+// // create row 1
+// const rowOne = document.createElement('div');
+// rowOne.className = 'row';
+// rowOne.id = 'row1';
+// mainContainerSelector.appendChild(rowOne);
+
+// // create divs in row
+// for (i = 1; i <= 16; i++) {
+//     const divCreator = document.createElement('div');
+//     divCreator.textContent = 'div';
+//     divCreator.className = 'box';
+//     rowOne.appendChild(divCreator);
+// }
+
+// // create row 2
+// const rowTwo = document.createElement('div');
+// rowTwo.className = 'row';
+// rowTwo.id = 'row2';
+// mainContainerSelector.appendChild(rowTwo);
+
+// // create divs in row
+// for (i = 1; i <= 16; i++) {
+//     const divCreator = document.createElement('div');
+//     divCreator.textContent = 'div';
+//     divCreator.className = 'box';
+//     rowTwo.appendChild(divCreator);
+// }
+
+for (i = 1; i <= 16; i++) {
     const rowCreator = document.createElement('div');
     rowCreator.className = 'row';
+    rowCreator.id = `row${i}`;
     mainContainerSelector.appendChild(rowCreator);
-}
 
-const rowSelector = document.querySelector('.row');
-
-// create 16 divs inside each row
-for (let i = 1; i <= 16; i++) {
-    const divCreator = document.createElement('div');
-    divCreator.textContent = 'div';
-    divCreator.className = 'box';
-    rowSelector.appendChild(divCreator);
+    for (j = 1; j <= 16; j++) {
+        const divCreator = document.createElement('div');
+        divCreator.textContent = 'div';
+        divCreator.className = 'box';
+        divCreator.id = `div${i}`;
+        rowCreator.appendChild(divCreator);
+    }
+    
 }
