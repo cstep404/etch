e8bb6af05c14717f581fc6b72d8b5e63c2e7b82a
Chip <cstep772@gmail.com>
2023-08-26 15:17:46 -0400

    Created 1 row with 16 div

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,19 @@ const mainContainerSelector = document.querySelector('#main-container');
 
 // create the element, manipulate the element, and then place it on page by appending
 
+// create 16 rows
 for (let i = 1; i <= 16; i++) {
-    const squareCreator = document.createElement('div');
-    squareCreator.textContent = `Box ${i}`
-    squareCreator.style.border = '2px solid black';
-    mainContainerSelector.appendChild(squareCreator);
+    const rowCreator = document.createElement('div');
+    rowCreator.className = 'row';
+    mainContainerSelector.appendChild(rowCreator);
+}
+
+const rowSelector = document.querySelector('.row');
+
+// create 16 divs inside each row
+for (let i = 1; i <= 16; i++) {
+    const divCreator = document.createElement('div');
+    divCreator.textContent = 'div';
+    divCreator.className = 'box';
+    rowSelector.appendChild(divCreator);
 }
